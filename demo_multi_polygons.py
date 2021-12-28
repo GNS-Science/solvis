218cd52ba939b330a9384337256816215f2224ba
Chris Chamberlain <chrisbc@artisan.co.nz>
2021-12-29 10:45:16 +1300

    refactor before threading

--- demo_multi_polygons.py
+++ demo_multi_polygons.py
@@ -5,9 +5,16 @@ import itertools
 import random
 
 from pathlib import PurePath
+import threading
+import concurrent.futures
 
 from solvis import *
 
+
+polygon_query_cache = {}
+polygon_lock = threading.Lock()
+combo_cache = {}
+combo_lock = threading.Lock()
 
 def export(sol):
     # #wlg_above_sol == new_sol(ri_sol, above)
@@ -43,7 +50,7 @@ def city_combinations(cities, pop_impacted=1e6, combo_max=5):
         combos.extend(sum_above([c for c in itertools.combinations(cities, rng)], cities, pop_impacted))
     return combos
 
-polygon_query_cache = {}
+
 
 def process(sol, cities, site_keys, radius):
     rupts_in_all_locs = set(sol.ruptures['Rupture Index'])
@@ -53,19 +60,20 @@ def process(sol, cities, site_keys, radius):
     for sk in site_keys:
         locations[sk] = cities[sk]
 
+    # print("locations", locations)
+    # assert 0
+
     for key, location in locations.items():
         #print(location)
 
-        # if key =="AK":
-        #     polygon = circle_polygon(radius_m=radius*2, lat=location[1], lon=location[2])
-        # else:
         polygon = circle_polygon(radius_m=radius, lat=location[1], lon=location[2])
 
         #cache the query result
         rupts = polygon_query_cache.get(f'{key}:{radius}', None)
         if rupts is None:
             rupts = sol.get_ruptures_intersecting(polygon)
-            polygon_query_cache[f'{key}:{radius}'] = rupts
+            with polygon_lock:
+                polygon_query_cache[f'{key}:{radius}'] = rupts
 
         print(f"city: {key}, radius: {radius} , Pop: {location[3]}, ruptures: {len(rupts)}")
         if len(rupts) == 0:
@@ -74,6 +82,41 @@ def process(sol, cities, site_keys, radius):
         rupts_in_all_locs = rupts_in_all_locs.intersection(set(rupts))
 
     return new_sol(sol, rupts_in_all_locs)
+
+
+def proc_radius(site_set, radius, rate_thresholds):
+
+    #cache the query result
+    key = ".".join(site_set[:-1])
+    rupts = combo_cache.get(f'{key}:{radius}', None)
+    if rupts  == 0:
+        print('skip cause no rupts here')
+        return
+    else:
+        subset = process(sol[1], cities, site_set, radius)
+        rates = subset.rates
+        with combo_lock:
+            combo_cache[f'{key}:{radius}'] = rates.shape[0]
+
+    print(f"{rates.shape[0]} events total for {site_set} ")
+    if rates.shape[0] == 0:
+        return
+
+    for threshold in rate_thresholds:
+        unique_events = set()
+        rated_events = rates[rates['Annual Rate']>threshold]
+        if rated_events.shape[0] >0:
+            print (f"{rated_events.shape[0]} events with rate > {threshold} for {site_set} ")
+            idxs = list(rated_events["Rupture Index"])
+            for idx in idxs:
+                if not idx in unique_events:
+                    # for site in site_set:
+                    yield (i, sol[0], site_set, radius, idx)
+        else:
+            return
+
+
+
 
 if __name__ == "__main__":
 
@@ -112,77 +155,31 @@ if __name__ == "__main__":
     radii.reverse()
     rate_thresholds.reverse()
 
-    # count = 0
-    # for (sol, site_set, radius, rate_threshold) in itertools.product(solutions, combos, radii, rate_thresholds):
-    #     count +=1
-    # print(f"{count} permutations")
+    count = 0
+    for (sol, site_set, radius, rate_threshold) in itertools.product(solutions, combos, radii, rate_thresholds):
+        count +=1
+    print(f"{count} permutations")
 
      #_df = pd.DataFrame(columns=['City Code', 'Site Radius', 'Rupture Index'])
-
+    out_file = f"DATA/rupts_sol({sol[0]}).csv"
     site_rupts = []
-    i = 0
+    i, writes = 0, 0
+
     for sol in solutions:
         for site_set in combos:
             print(f"{site_set}")
             site_radius_rupts = []
             for radius in radii:
-                subset = process(sol[1], cities, site_set, radius)
-                rates = subset.rates
-
-                print(f"{rates.shape[0]} events total for {site_set} ")
-                if rates.shape[0] == 0:
-                    continue
-
-                unique_events = set()
-                for threshold in rate_thresholds:
-                    rated_events = rates[rates['Annual Rate']>threshold]
-                    if rated_events.shape[0] >0:
-                        print (f"{rated_events.shape[0]} events with rate > {threshold} for {site_set} ")
-                        idxs = list(rated_events["Rupture Index"])
-                        for idx in idxs:
-                            if not idx in unique_events:
-                                # for site in site_set:
-                                row = (i, sol[0], site_set, radius, idx)
-                                site_radius_rupts.append(row)
-                                #print("STORE", i, sol[0], site, radius, idx)
-                                i+=1
-                                unique_events.add(idx)
-                    else:
-                        continue
+                for row in proc_radius(site_set, radius, rate_thresholds):
+                    site_radius_rupts.append(row)
+                    i+=1
 
             #site_set dataframe
             df = pd.DataFrame(site_radius_rupts, columns=["Index", 'Solution ID', 'City Codes', 'Site Radius', 'Rupture Index'])
-            df.to_csv(f"DATA/site_rupts_sol({sol[0]})_{'-'.join(site_set)}.csv")
-            site_rupts.extend(site_radius_rupts)
+            if writes == 0:
+                df.to_csv(out_file)
+            else:
+                df.to_csv(out_file, mode='a', header=False)
+            writes +=1
 
-        #sol dataframe
-        df = pd.DataFrame(site_rupts, columns=["Index", 'Solution ID', 'City Codes', 'Site Radius', 'Rupture Index'])
-        df.to_csv(f"DATA/rupts_sol({sol[0]}).csv")
 
-
-    """
-    print(f"Build Filtered Inversion Solutions for source {name}.")
-    print()
-    print(f"with {sol.rates[sol.rates['Annual Rate']>0].shape[0]} ruptures with rate>0.")
-    print()
-
-    # site_sets = [('AK', 'WN'), ("WN", "AK", "CC")]
-
-    def get_locations(site_set, cities):
-        locations = dict()
-        #print(site_set)
-        for key, city in cities.items():
-            #print(key)
-            if key in site_set:
-                locations[key] = city
-        return locations
-
-    for site_set in site_sets:
-        locs = get_locations(site_set, cities)
-        #print(site_set)
-        rates = process(sol, locs).rates
-        rate_filters = [0, 1e-15, 1e-12, 1e-9, 1e-6]
-        for threshold in rate_filters:
-            print (f"{rates[rates['Annual Rate']>threshold].shape[0]} events with rate > {threshold} for {site_set} ")
-
-    """
