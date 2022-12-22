0e598a124df7f5d45ec5f618e28792f42ef061a7
Chris Chamberlain <chrisbc@artisan.co.nz>
2022-12-22 17:35:11 +1300

    fix __version__

--- solvis/__init__.py
+++ solvis/__init__.py
@@ -1,4 +1,4 @@
 from .inversion_solution import InversionSolution
 from .solvis import circle_polygon, export_geojson, mfd_hist, new_sol, rupt_ids_above_rate, section_participation
 
-__version__ = '0.1.0'
+__version__ = '0.3.0'
