7ec087860f4cd2042894d5fa9aa55bc239a009e6
Chris Chamberlain <chrisbc@artisan.co.nz>
2023-02-21 08:47:54 +1300

    Bump version: 0.3.1 → 0.4.0

--- solvis/__init__.py
+++ solvis/__init__.py
@@ -2,4 +2,4 @@ from .geometry import circle_polygon
 from .inversion_solution import InversionSolution
 from .solvis import export_geojson, mfd_hist, new_sol, rupt_ids_above_rate, section_participation
 
-__version__ = '0.3.1'
+__version__ = '0.4.0'
