1d0cc21fd62c4ca0f5adce1600f04013e263621d
Chris Chamberlain <chrisbc@artisan.co.nz>
2024-07-03 15:58:49 +1200

    manual version bump

--- solvis/__init__.py
+++ solvis/__init__.py
@@ -29,4 +29,4 @@ from .geometry import circle_polygon
 from .inversion_solution import CompositeSolution, FaultSystemSolution, InversionSolution
 from .solvis import export_geojson, mfd_hist, parent_fault_names, rupt_ids_above_rate, section_participation
 
-__version__ = '0.12.0-alpha'
+__version__ = '0.12.0-alpha.2'
