01021cfb47c2c51f116c5947bf7c0e8a68176d7c
Chris Chamberlain <chrisbc@artisan.co.nz>
2024-07-30 13:41:55 +1200

    drop_zero_rate differentiates between FSS and IS;

--- solvis/fault_system_solution_helper.py
+++ solvis/fault_system_solution_helper.py
@@ -2,6 +2,7 @@ from collections import namedtuple
 from typing import Iterator, Set
 
 import solvis.inversion_solution.typing
+from solvis.inversion_solution import FaultSystemSolution
 
 """
 NAMES
@@ -38,20 +39,20 @@ class FaultSystemSolutionHelper:
         get Parent_fault_ids for the given parent fault names
         """
         df0 = self._fss.fault_sections
-        print("fault_sections")
-        print(df0[["ParentID", "ParentName"]])
+        # print("fault_sections")
+        # print(df0[["ParentID", "ParentName"]])
         ids = df0[df0['ParentName'].isin(list(fault_names))]['ParentID'].tolist()
         return set([int(id) for id in ids])
 
     def parent_fault_name_id_mapping(self, parent_ids: Set[int]) -> Iterator[ParentFaultMapping]:
         df0 = self._fss.fault_sections
-        print("fault_sections")
-        print(df0[["ParentID", "ParentName"]])
+        # print("fault_sections")
+        # print(df0[["ParentID", "ParentName"]])
         df1 = df0[df0['ParentID'].isin(list(parent_ids))][['ParentID', 'ParentName']]
         unique_ids = list(df1.ParentID.unique())
         unique_names = list(df1.ParentName.unique())
         for idx, parent_id in enumerate(unique_ids):
-            print(idx, parent_id, unique_names[idx])
+            # print(idx, parent_id, unique_names[idx])
             yield ParentFaultMapping(parent_id, unique_names[idx])
 
     def fault_names_as_ids(self, fault_names: Set[str]):
@@ -78,17 +79,19 @@ class FaultSystemSolutionHelper:
         get all ruptures (by id) on the given faults
 
         TODO: any (INTERSECTION) or all (UNION)
-
         """
         subsection_ids = self.subsections_for_faults(parent_ids)
         df0 = self._fss.rupture_sections
-        print(df0)
+        # print(df0)
+        print(self._fss.rupture_rates)
+
+        # TODO this is needed becuase the rupture rate concept differs between IS and FSS classes
+        rate_column = "rate_weighted_mean" if isinstance(self._fss, FaultSystemSolution) else "Annual Rate"
         if drop_zero_rates:
             df0 = df0.join(self._fss.rupture_rates.set_index("Rupture Index"), on='rupture', how='inner')[
-                ["rupture", "section"]
+                [rate_column, "rupture", "section"]
             ]
-            # print("inner")
-            # print(df0)
+            df0 = df0[df0[rate_column] >0]
 
         ids = df0[df0['section'].isin(list(subsection_ids))]['rupture'].tolist()
         return set([int(id) for id in ids])
@@ -118,7 +121,7 @@ def section_participation_rate(solution: solvis.InversionSolution, section: int)
     """
     get the 'participation rate" of a (sub)section.
 
-    This is the sum of the rate of all ruptures that involve the requested section.
+    That is, the sum of rates for all ruptures that involve the requested section.
     """
     helper = FaultSystemSolutionHelper(solution)
     ruptures = helper.ruptures_for_subsections([section])
@@ -128,8 +131,15 @@ def section_participation_rate(solution: solvis.InversionSolution, section: int)
 
 
 def fault_participation_rate(solution: solvis.InversionSolution, fault_name: str):
+    """
+    get the 'participation rate" of a given parent fault.
+
+    That is, the sum of rates for all ruptures that involve the requested parent fault .
+    """
     helper = FaultSystemSolutionHelper(solution)
     ruptures = helper.ruptures_for_parent_fault_names([fault_name])
     # ruptures = {8, 9, 10}  # mocking helper.ruptures_given_subsections()
     df = solution.ruptures_with_rupture_rates[["Rupture Index", "Annual Rate"]]
     return df[df["Rupture Index"].isin(list(ruptures))]["Annual Rate"].sum()
+
+
