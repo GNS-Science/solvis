f5a3261279b2411795482492daeead5c52904697
Chris DiCaprio <christopher.dicaprio@gmail.com>
2025-02-10 15:23:45 +1300

    update pytz dep (#69)
    * update pytz, nzshm-model, mkdocstrings-python dependencies
    * handle tests and imports when optional dependencies not present

--- solvis/dochelper/__init__.py
+++ solvis/dochelper/__init__.py
@@ -1,6 +1,5 @@
 """Package supporting dynamic dosstrings."""
 
-from .extension import DynamicDocstrings
 from .inherit_docstrings import inherit_docstrings
 
-__all__ = ["DynamicDocstrings", "inherit_docstrings"]
+__all__ = ["inherit_docstrings"]
