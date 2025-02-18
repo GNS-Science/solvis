1be9d67a7eaefdba12402592c4374f5c84e0490a
Chris Chamberlain <chrisbc@artisan.co.nz>
2025-02-18 15:49:57 +1300

    detox;

--- test/inversion_solution/test_inversion_solution_rate_scaling.py
+++ test/inversion_solution/test_inversion_solution_rate_scaling.py
@@ -1,8 +1,10 @@
 #!python3 test_inversion_solution_rate_scaling.py
 
 import pytest
+from nzshm_common.location.location import location_by_id
 
-from solvis import InversionSolution
+from solvis import InversionSolution, circle_polygon
+from solvis.filter.rupture_id_filter import FilterRuptureIds
 
 
 @pytest.mark.parametrize("scale", [0.1, 0.55, 0.89, 1.5])
@@ -21,7 +23,7 @@ def test_scale_rupture_rates_uniformly(puysegur_small_fixture, scale):
 
 
 @pytest.mark.parametrize("magnitude", [1.5, 4.2, 6.5, 7.25, 9.55])
-def test_scale_rupture_rates_below_magniture(puysegur_small_fixture, magnitude):
+def test_scale_rupture_rates_below_magnitude(puysegur_small_fixture, magnitude):
     sol = puysegur_small_fixture
     scale = 0.5
     new_sol = InversionSolution.scale_rupture_rates(solution=sol, scale=scale, max_magnitude=magnitude)
@@ -46,3 +48,17 @@ def test_scale_rupture_rates_below_magniture(puysegur_small_fixture, magnitude):
         assert (
             new_sol.solution_file.rupture_rates["Annual Rate"] * 1 / scale
         ).sum() == sol.solution_file.rupture_rates["Annual Rate"].sum()
+
+
+@pytest.mark.skip('WIP')
+def test_scale_rupture_rates_for_polygon(crustal_solution_fixture):
+
+    WLG = location_by_id('WLG')
+    polygon = circle_polygon(5e4, WLG['latitude'], WLG['longitude'])  # 50km circle around WLG
+    rupture_ids = FilterRuptureIds(crustal_solution_fixture, drop_zero_rates=False).for_polygon(polygon)
+    all_rupture_ids = FilterRuptureIds(crustal_solution_fixture, drop_zero_rates=False).all()
+
+    assert all_rupture_ids.issuperset(rupture_ids)
+
+    # TODO: added test coverage and implmenent the feature
+    assert 0
