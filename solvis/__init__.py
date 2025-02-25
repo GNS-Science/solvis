a389bf5cc3e5a42765b59024c191de86889ff467
Chris Chamberlain <chrisbc@artisan.co.nz>
2025-02-25 13:46:09 +1300

    align version ids

--- solvis/__init__.py
+++ solvis/__init__.py
@@ -26,4 +26,4 @@ from .geometry import circle_polygon
 from .solution import CompositeSolution, FaultSystemSolution, InversionSolution
 from .solvis import export_geojson, mfd_hist
 
-__version__ = '1.0.0-beta'
+__version__ = '1.0.0-beta-0'
